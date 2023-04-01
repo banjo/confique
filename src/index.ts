3d89f5062e6f76f80a8bafc3c37155e1eebf40de
Anton Ödman <anton.odman@gmail.com>
2023-04-01 23:28:51 +0200

    feat: add option to prefer extension

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
-import { includes } from "@banjoanton/utils";
+import { includes, sortBy } from "@banjoanton/utils";
 import { Jiti } from "./parsers/jiti";
 import { Yaml } from "./parsers/yaml";
 
 const SUPPORTED_JITI_EXTENSIONS = ["json", "ts", "js", "cjs", "mjs"] as const;
 const SUPPORTED_YAML_EXTENSIONS = ["yaml", "yml"] as const;
+const PACKAGE_JSON = "package.json" as const;
+
+const SUPPORTED_EXTENSIONS = [
+    ...SUPPORTED_JITI_EXTENSIONS,
+    ...SUPPORTED_YAML_EXTENSIONS,
+    ...PACKAGE_JSON,
+];
+type SupportedExtensions = (typeof SUPPORTED_EXTENSIONS)[number];
 
 const getExtension = (path: string): string => {
     if (path.endsWith("rc")) {
@@ -23,6 +31,7 @@ type Options = {
     searchPaths?: string[];
     throw?: boolean;
     export?: string;
+    preferOrder?: SupportedExtensions[];
 };
 
 const defaultSearchPaths = (libraryName: string) => [
@@ -74,7 +83,25 @@ const config = (libraryName: string, options?: Options) => {
     };
 
     const search = async () => {
-        for (const path of searchPaths) {
+        const paths = sortBy(searchPaths, path => {
+            const preferredOrder = options?.preferOrder || [];
+
+            const extension = getExtension(path);
+
+            if (!includes(SUPPORTED_EXTENSIONS, extension)) {
+                return Number.MAX_SAFE_INTEGER;
+            }
+
+            if (!includes(preferredOrder, extension as SupportedExtensions)) {
+                return Number.MAX_SAFE_INTEGER;
+            }
+
+            const result = preferredOrder.indexOf(extension as SupportedExtensions);
+
+            return result;
+        });
+
+        for (const path of paths) {
             const loadedConfig = await load(path);
             if (loadedConfig) return loadedConfig;
         }
@@ -92,6 +119,6 @@ const config = (libraryName: string, options?: Options) => {
     };
 };
 
-const test = config("test", { export: "default", searchPaths: [".testrc"] });
+const test = config("test", { export: "default", preferOrder: ["ts", "package.json"] });
 const result = await test.search();
 console.log(result);
